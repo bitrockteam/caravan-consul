094861074a4b8a921b2da5211cc528b2a60bd0f7
efbar <17676301+efbar@users.noreply.github.com>
2020-06-16 15:59:59 +0200

    add connection to provisioner....

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -10,7 +10,14 @@ resource "null_resource" "consul_cluster_node" {
         cluster_nodes = var.cluster_nodes
       }
     )}
-  EOT
+    EOT
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = var.ssh_private_key
+      timeout     = var.ssh_timeout
+      host        = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[each.key] : each.value
+    }
   }
 
   provisioner "remote-exec" {
