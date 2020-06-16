afecb98913f9608dc0ef86fd1d66c1cf7cc9a48f
efbar <17676301+efbar@users.noreply.github.com>
2020-06-16 15:48:51 +0200

    add consul config handling

--- modules/consul-cluster/consul-cluster.hcl.tpl
+++ modules/consul-cluster/consul-cluster.hcl.tpl
@@ -1,11 +1,9 @@
-{
-    "datacenter": "hcpoc",
-    "data_dir": "/var/lib/consul",
-    "log_level": "INFO",
-    "node_name": "${}",
-    "server": true,
-    "telemetry": {
-       "statsite_address": "127.0.0.1:2180"
-    }
-  }
+datacenter = "hcpoc"
+data_dir = "/var/lib/consul"
+log_level = "INFO"
+node_name = "${cluster_nodes}"
+server = true
+telemetry = {
+   statsite_address = "127.0.0.1:2180"
+}
   
--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -1,5 +1,29 @@
 resource "null_resource" "consul_cluster_node" {
   for_each = var.cluster_nodes
+
+  provisioner "file" {
+    destination = "/tmp/consul.hcl"
+    content = <<-EOT
+    ${templatefile(
+      "${path.module}/consul-cluster.hcl.tpl",
+      {
+        cluster_nodes = var.cluster_nodes
+      }
+    )}
+  EOT
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo mv /tmp/consul.hcl /etc/consul.d/consul.hcl"]
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      timeout     = var.ssh_timeout
+      private_key = var.ssh_private_key
+      host        = var.cluster_nodes_public_ips != null ? var.cluster_nodes_public_ips[each.key] : each.value
+    }
+  }
+
   provisioner "remote-exec" {
     script = "${path.module}/scripts/consul_cluster_init.sh"
     connection {
