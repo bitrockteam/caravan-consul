0f7387ab3b983183cdd66e5b0b549307a4f76194
efbar <17676301+efbar@users.noreply.github.com>
2020-06-16 14:34:57 +0200

    first lines for consul provisioning

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -1,3 +1,13 @@
 resource "null_resource" "consul_cluster_node" {
   for_each = var.cluster_nodes
+  provisioner "remote-exec" {
+    script = "${path.module}/scripts/consul_cluster_init.sh"
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      timeout     = var.ssh_timeout
+      private_key = var.ssh_private_key
+      host        = var.cluster_nodes_public_ips[keys(var.cluster_nodes)[0]]
+    }
+  }
 }
