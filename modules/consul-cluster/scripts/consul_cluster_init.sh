c592fc69e3c3c6e73280ac3b374736b144f47cd1
efbar <17676301+efbar@users.noreply.github.com>
2020-06-17 15:18:16 +0200

    get bootstrap token

--- modules/consul-cluster/scripts/consul_cluster_init.sh
+++ modules/consul-cluster/scripts/consul_cluster_init.sh
@@ -3,4 +3,6 @@ set -e
 sudo ls -la /etc/consul.d/
 sudo systemctl start consul &&  \
 sleep 10s && \
-systemctl status consul
+systemctl status consul && \
+awk '/SecretID/{print $2}' | \
+sudo tee /root/bootstrap_token
