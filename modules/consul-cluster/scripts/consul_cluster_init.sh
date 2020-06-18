b151e6c07ea727d221458a256842857396255405
efbar <17676301+efbar@users.noreply.github.com>
2020-06-18 11:52:29 +0200

    boostrap and get token

--- modules/consul-cluster/scripts/consul_cluster_init.sh
+++ modules/consul-cluster/scripts/consul_cluster_init.sh
@@ -5,4 +5,9 @@ sudo systemctl start consul &&  \
 sleep 10s && \
 systemctl status consul && \
 awk '/SecretID/{print $2}' | \
-sudo tee /root/bootstrap_token
+sudo tee /root/bootstrap_token && \
+export CONSUL_HTTP_TOKEN=`sudo cat /root/bootstrap_token` 
+# && \
+# consul acl policy create -name cluster-node-1-agent -rules @cluster-node-1.hcl && \
+# consul acl policy create -name cluster-node-2-agent -rules @cluster-node-2.hcl && \
+# consul acl policy create -name cluster-node-3-agent -rules @cluster-node-3.hcl && \
