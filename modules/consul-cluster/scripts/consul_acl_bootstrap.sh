1ae7839a33c7164872c794f4dc9b6f8112fd4c20
efbar <17676301+efbar@users.noreply.github.com>
2020-06-19 20:05:30 +0200

    monters, stay away

--- modules/consul-cluster/scripts/consul_acl_bootstrap.sh
+++ modules/consul-cluster/scripts/consul_acl_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-consul acl bootstrap && \
+consul acl bootstrap | \
 awk '/SecretID/{print $2}' | \
-sudo tee /root/bootstrap_token &&
+sudo tee /root/bootstrap_token && \
 sleep 5s
