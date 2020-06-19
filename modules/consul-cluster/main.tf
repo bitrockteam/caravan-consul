8de65e5074cedcb0503a7fa1435a2ed04b7733d1
efbar <17676301+efbar@users.noreply.github.com>
2020-06-19 17:59:47 +0200

    another fix

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -66,7 +66,7 @@ resource "null_resource" "consul_cluster_node_1_init" {
 resource "null_resource" "consul_cluster_not_node_1_init" {
   count = length(var.cluster_nodes) - 1 < 0 ? 0 : length(var.cluster_nodes) - 1
   triggers = {
-    nodes = null_resource.consul_cluster_node_1_init[keys(var.cluster_nodes)[0]].id
+    nodes = null_resource.consul_cluster_node_1_init
   }
 
   provisioner "remote-exec" {
