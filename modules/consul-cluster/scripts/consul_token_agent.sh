7587ad2fc7532b76654bacd42d3d450a65ea4fd7
efbar <17676301+efbar@users.noreply.github.com>
2020-06-19 21:15:53 +0200

    save ui token

--- modules/consul-cluster/scripts/consul_token_agent.sh
+++ modules/consul-cluster/scripts/consul_token_agent.sh
@@ -2,4 +2,6 @@ export CONSUL_HTTP_TOKEN=`sudo cat /root/bootstrap_token` && \
 consul acl policy create -name "agent-token" -description "Agent Token Policy" -rules @cluster-node-agent.hcl && \
 consul acl token create -description "Agent Token" -policy-name "agent-token"
 consul acl policy create -name "ui-token" -description "UI Token Policy" -rules @operator-ui.hcl && \
-consul acl token create -description "UI Token" -policy-name "ui-token"
+consul acl token create -description "UI Token" -policy-name "ui-token"|
+awk '/SecretID/{print $2}' | \
+sudo tee /root/ui_token
