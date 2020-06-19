828ee4a6551a3ea4762c57e04818a7dfcd343cf5
efbar <17676301+efbar@users.noreply.github.com>
2020-06-19 15:47:41 +0200

    remove useless lines

--- modules/consul-cluster/scripts/consul_cluster_init.sh
+++ modules/consul-cluster/scripts/consul_cluster_init.sh
@@ -3,8 +3,4 @@ set -e
 sudo ls -la /etc/consul.d/ && \
 sudo systemctl start consul &&  \
 sleep 10s && \
-systemctl status consul 
-# && \
-# consul acl policy create -name cluster-node-1-agent -rules @cluster-node-1.hcl && \
-# consul acl policy create -name cluster-node-2-agent -rules @cluster-node-2.hcl && \
-# consul acl policy create -name cluster-node-3-agent -rules @cluster-node-3.hcl && \
+systemctl status consul
