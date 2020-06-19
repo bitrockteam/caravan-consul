68ec426dd1ad8ebe164b55062154eecb7e014494
efbar <17676301+efbar@users.noreply.github.com>
2020-06-19 13:56:23 +0200

    speechless!

--- modules/consul-cluster/scripts/consul_token_agent.sh
+++ modules/consul-cluster/scripts/consul_token_agent.sh
@@ -1,4 +1,4 @@
-sudo cd /etd/consul.d/acls && \
+sudo cd /etc/consul.d/acls && \
 export CONSUL_HTTP_TOKEN=`sudo cat /root/bootstrap_token` && \
 consul acl policy create -name "agent-token" -description "Agent Token Policy" -rules @cluster-node-agent.hcl && \
 consul acl token create -description "Agent Token" -policy-name "agent-token"
