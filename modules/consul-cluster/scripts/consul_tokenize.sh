fd7d1e6f1421d59fc0c067dea20efec5b0ac208f
efbar <17676301+efbar@users.noreply.github.com>
2020-06-29 23:22:45 +0200

    one token is forever

--- modules/consul-cluster/scripts/consul_tokenize.sh
+++ modules/consul-cluster/scripts/consul_tokenize.sh
@@ -1,6 +1,6 @@
 export CONSUL_HTTP_TOKEN=`sudo cat /root/bootstrap_token` && \
 consul acl policy create -name "agent-token" -description "Agent Token Policy" -rules @cluster-node-agent.hcl && \
-consul acl token create -description "Agent Token" -policy-name "agent-token-0"
+consul acl token create -description "Agent Token" -policy-name "agent-token"
 consul acl policy create -name "ui-token" -description "UI Token Policy" -rules @operator-ui.hcl && \
 consul acl token create -description "UI Token" -policy-name "ui-token"|
 awk '/SecretID/{print $2}' | \
