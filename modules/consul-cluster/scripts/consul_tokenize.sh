99f46da1d921557d2073df5dfb3fe1ed071608aa
efbar <17676301+efbar@users.noreply.github.com>
2020-06-29 23:52:40 +0200

    crying

--- modules/consul-cluster/scripts/consul_tokenize.sh
+++ modules/consul-cluster/scripts/consul_tokenize.sh
@@ -4,7 +4,7 @@ consul acl token create -description "Agent Token" -policy-name "agent-token"
 consul acl policy create -name "ui-token" -description "UI Token Policy" -rules @operator-ui.hcl && \
 consul acl token create -description "UI Token" -policy-name "ui-token"|
 awk '/SecretID/{print $2}' | \
-sudo tee /root/ui_token
+sudo tee /root/ui_token && \
 export `sudo sh /root/vault.vars` && \
 vault kv put secret/consul/ui_token token="`sudo cat /root/ui_token`" && \
 sudo rm /root/ui_token
