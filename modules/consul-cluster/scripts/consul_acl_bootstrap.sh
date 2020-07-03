d1f42382e83bfe1a661c57e7d7a4b19fff2625b6
efbar <17676301+efbar@users.noreply.github.com>
2020-07-03 18:19:47 +0200

    add some

--- modules/consul-cluster/scripts/consul_acl_bootstrap.sh
+++ modules/consul-cluster/scripts/consul_acl_bootstrap.sh
@@ -4,5 +4,5 @@ consul acl bootstrap | \
 awk '(/Secret/ || /Accessor/)'| sudo tee /root/tokens && \
 sleep 5s
 export `sudo sh /root/vault.vars` && \
-vault kv put secret/consul/bootstrap_token secretid="`sudo cat /root/tokens | awk '/Secret/{print $2}'`"
+vault kv put secret/consul/bootstrap_token secretid="`sudo cat /root/tokens | awk '/Secret/{print $2}'`" && \
 vault kv put secret/consul/bootstrap_token accessorid="`sudo cat /root/tokens | awk '/Access/{print $2}'`"
