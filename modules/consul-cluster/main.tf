1ef0f8f8100b4d22ccb408dec724eca924a86a03
efbar <17676301+efbar@users.noreply.github.com>
2020-07-13 16:06:53 +0200

    remove star

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -39,7 +39,7 @@ provisioner "file" {
 }
 
 provisioner "remote-exec" {
-  inline = ["sudo mv vault.vars /root/vault.vars; sudo mv /tmp/consul*.hcl /etc/consul.d/consul.hcl"]
+  inline = ["sudo mv vault.vars /root/vault.vars; sudo mv /tmp/consul.hcl /etc/consul.d/consul.hcl"]
   connection {
     type        = "ssh"
     user        = var.ssh_user
