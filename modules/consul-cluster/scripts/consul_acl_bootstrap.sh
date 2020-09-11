c6a275be0244253c1d71f72f294578e8d733865c
gfdsa <mtabolsky@gmail.com>
2020-09-11 11:18:17 +0200

    Do not kv put the empty strings (fixes #2)

--- modules/consul-cluster/scripts/consul_acl_bootstrap.sh
+++ modules/consul-cluster/scripts/consul_acl_bootstrap.sh
@@ -4,5 +4,7 @@ consul acl bootstrap | \
 awk '(/Secret/ || /Accessor/)'| sudo tee /root/tokens && \
 sleep 5s
 export `sudo sh /root/vault.vars` && \
-vault kv put secret/consul/bootstrap_token secretid="`sudo cat /root/tokens | awk '/Secret/{print $2}'`" accessorid="`sudo cat /root/tokens | awk '/Access/{print $2}'`" && \
+{ [ -z "`sudo cat /root/tokens | awk '/Secret/{print $2}'`" ] ||
+  vault kv put secret/consul/bootstrap_token secretid="`sudo cat /root/tokens | awk '/Secret/{print $2}'`" accessorid="`sudo cat /root/tokens | awk '/Access/{print $2}'`"
+} && \
 sudo rm -f /root/tokens
