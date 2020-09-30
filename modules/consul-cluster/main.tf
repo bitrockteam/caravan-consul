092047c40912ff2414760724d0de69bdc81073c3
efbar <17676301+efbar@users.noreply.github.com>
2020-09-30 16:58:59 +0200

    zzzzzzz

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -63,7 +63,7 @@ resource "null_resource" "consul_cluster_node_deploy_config" {
     "${path.module}/consul-server.hcl.tmpl",
     {
       cluster_nodes = var.cluster_nodes
-      node_id       = each.key,
+      node_id       = each.key
       dc_name       = var.dc_name
     }
 )}
@@ -87,7 +87,7 @@ provisioner "file" {
   "${path.module}/consul-server.hcl",
   {
     cluster_nodes = var.cluster_nodes
-    node_id       = each.key,
+    node_id       = each.key
     dc_name       = var.dc_name
   }
 )}
